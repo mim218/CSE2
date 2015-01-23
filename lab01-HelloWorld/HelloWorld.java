4649c49afe6042f48c1721b512c69583a2014f0a
Michaela Martines <mim218@lehigh.edu>
2015-01-23 19:49:12 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+//Michaela Martines
+//Demo 1
+//Hello World Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld//
+
+//  define class
+public class HelloWorld{
+    
+//  add main method
+    public static void main(String[] args) {
+
+//  print the statement with box around it
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("#                      #");
+    System.out.println("#  Hellow World 2014!! #");
+    System.out.println("#                      #");
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("test");
+    
+    //test/testtest
+    }
+}
