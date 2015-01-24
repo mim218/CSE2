54daebadd940803e9d3510896931b83695d1776b
Michaela Martines <mim218@lehigh.edu>
2015-01-24 18:08:07 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+//Michaela Martines
+//Homework 1
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//  print the statement with box around it and add autobio. at end
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("^ ^ ^ ^ ^ ^");
+    System.out.println("/\\/\\/\\/\\/\\/\\");
+    System.out.println("<-M--I--M--2--1--8->");
+    System.out.println("\\/\\/\\/\\/\\/\\/");
+    System.out.println(" v v v v v ");
+    System.out.println("Over winter break, I spent a lot of time with my family.");
+    
+    //test the system
+    }
+}
